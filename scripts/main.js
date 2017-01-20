69f113d1ede8b142ccb60dd644f5e460f4f95177
Nataliya_Satsyuk <natalia.satsyuk@gmail.com>
2017-01-20 12:48:34 +0200

    init

---
+++ scripts/main.js
@@ -0,0 +1,133 @@
+// slide-menu
+
+$(".toggle-button").click(function() {
+	$(".nav-list").slideToggle(400);
+});
+
+
+
+// slider
+
+$(".offer-slider-wrap").slick({
+	infinite: true,
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	//autoplay: true,
+	autoplaySpeed: 2000,
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3
+			}
+		},
+
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+	]
+});
+
+$(".windows-slider-wrap").slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	dots: true,
+	//autoplay: true,
+	autoplaySpeed: 2000,
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 3
+			}
+		},
+		{
+			breakpoint: 780,
+			settings: {
+				slidesToShow: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+	]
+});
+
+$(".stuff-slider-wrap").slick({
+	infinite: true,
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	dots: true,
+	//autoplay: true,
+	autoplaySpeed: 2000,
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+	]
+});
+
+$(".news-slider-wrap").slick({
+	infinite: true,
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	dots: true,
+	arrows: false,
+	//autoplay: true,
+	autoplaySpeed: 2000,
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+	]
+});
+
